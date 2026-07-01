3e531ac904e678669f522f39f64b11e68a7df92e
Jan Lafferton <jan@lafferton.de>
2026-07-01 10:15:09 +0200

    Remove unused plugin

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -15,37 +15,23 @@
       packages.neovim = inputs.wrapper-modules.wrappers.neovim.wrap {
         inherit pkgs;
         specs = {
-          start =
-            with pkgs.vimPlugins;
-            let
-              direnv-nvim = pkgs.vimUtils.buildVimPlugin {
-                name = "direnv.nvim";
-                src = pkgs.fetchFromGitHub {
-                  owner = "NotAShelf";
-                  repo = "direnv.nvim";
-                  rev = "564146278b3d5fe4ffa389cd103bab20f9b515d6";
-                  hash = "sha256-JbnGoZMApLtq4lSdGolcpKdsneolSOrzIi+O5yR2NDQ=";
-                };
-              };
-            in
-            [
-              flash-nvim
-              fzf-lua
-              live-rename-nvim
-              nvim-lspconfig
-              nvim-treesitter-context
-              nvim-treesitter-textobjects
-              nvim-treesitter.withAllGrammars
-              nvim-surround
-              oil-nvim
-              vim-tmux-navigator
-              blink-indent
-              blink-cmp
-              blink-pairs
-              sidekick-nvim
-              direnv-nvim
-              persistence-nvim
-            ];
+          start = with pkgs.vimPlugins; [
+            flash-nvim
+            fzf-lua
+            live-rename-nvim
+            nvim-lspconfig
+            nvim-treesitter-context
+            nvim-treesitter-textobjects
+            nvim-treesitter.withAllGrammars
+            nvim-surround
+            oil-nvim
+            vim-tmux-navigator
+            blink-indent
+            blink-cmp
+            blink-pairs
+            sidekick-nvim
+            persistence-nvim
+          ];
         };
         settings = {
           aliases = [
