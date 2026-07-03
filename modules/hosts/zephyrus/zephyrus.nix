a8c8486226b33b4feef898a29e578685debb34ae
Jan Lafferton <jan@lafferton.de>
2026-07-03 23:08:11 +0200

    Enable podman

--- modules/hosts/zephyrus/zephyrus.nix
+++ modules/hosts/zephyrus/zephyrus.nix
@@ -93,6 +93,12 @@
         zed-editor
       ];
 
+      virtualisation = {
+        libvirtd.enable = true;
+        containers.enable = true;
+        podman.enable = true;
+      };
+
       # Some programs need SUID wrappers, can be configured further or are
       # started in user sessions.
       # programs.mtr.enable = true;
