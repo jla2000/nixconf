9116b46bf38e573abd056c188d925284677f2055
Jan Lafferton <jan@lafferton.de>
2026-07-10 10:24:30 +0200

    Refactor herdr config

--- modules/herdr.nix
+++ modules/herdr.nix
@@ -1,21 +1,41 @@
-{ self, inputs, ... }:
+{ inputs, ... }:
 {
   flake.nixosModules.herdr =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
+    let
+      settings = {
+        onboarding = false;
+        theme.name = config.stylix.colorscheme;
+        keys = {
+          prefix = "ctrl+s";
+          split_vertical = "prefix+%";
+          split_horizontal = "prefix+\"";
+          command = [
+            {
+              key = "prefix+g";
+              type = "pane";
+              command = "jjui";
+              description = "run jjui";
+            }
+          ];
+        };
+        ui = {
+          prompt_new_tab_name = false;
+          pane_gaps = false;
+        };
+        experimental.pane_history = false;
+      };
+    in
     {
-      environment.systemPackages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.herdr ];
-    };
-
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.herdr = inputs.wrapper-modules.lib.wrapPackage (
-        { ... }:
-        {
-          inherit pkgs;
-          package = pkgs.herdr;
-          env.HERDR_CONFIG_PATH = ../config/herdr/config.toml;
-        }
-      );
+      environment.systemPackages = [
+        (inputs.wrapper-modules.lib.wrapPackage (
+          { ... }:
+          {
+            inherit pkgs;
+            package = pkgs.herdr;
+            env.HERDR_CONFIG_PATH = (pkgs.formats.toml { }).generate "herdr-config.toml" settings;
+          }
+        ))
+      ];
     };
 }
