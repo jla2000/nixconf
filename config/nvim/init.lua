a444542955f3d9c00a4a4fdeb982012424e2e4d5
Jan Lafferton <jan@lafferton.de>
2026-08-19 19:41:17 +0200

    Add aerynOS neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,84 +1,97 @@
 vim.g.mapleader = " "
--- <C-h/j/k/l> is owned by vim-herdr-navigation (falls back to tmux/wincmd)
-vim.g.tmux_navigator_no_mappings = 1
 
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.undofile = true
-vim.opt.shiftwidth = 2
-vim.opt.smarttab = true
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.signcolumn = "yes:1"
-vim.opt.scrolloff = 8
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.confirm = true
+vim.o.number = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.confirm = true
+vim.o.cursorline = true
+vim.o.signcolumn = "yes:1"
+vim.o.scrolloff = 8
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.wrap = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldtext = ""
-vim.opt.foldlevel = 99
 vim.opt.jumpoptions = "stack"
-vim.opt.grepprg = "rg --vimgrep --hidden -g '!.git/*'"
 vim.opt.termguicolors = true
--- vim.opt.timeoutlen = 300
 
-vim.cmd.packadd("cfilter")
-vim.cmd.packadd("nvim.undotree")
+vim.cmd.colorscheme("catppuccin")
 
-local on_jump = function(diagnostic, bufnr)
-  if not diagnostic then
-    return
-  end
-  vim.diagnostic.show(diagnostic.namespace, bufnr, { diagnostic }, {
-    virtual_lines = { current_line = true },
-  })
-end
-vim.diagnostic.config({ jump = { on_jump = on_jump } })
-
-vim.keymap.set("n", "<esc>", "<cmd>nohl<cr><esc>")
-vim.keymap.set("n", "<tab>", "<cmd>bn<cr>")
-vim.keymap.set("n", "<s-tab>", "<cmd>bp<cr>")
-vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>")
-
-vim.keymap.set("v", ">", ">gv")
-vim.keymap.set("v", "<", "<gv")
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.pack.add({
+  "https://github.com/saecki/live-rename.nvim",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/folke/flash.nvim",
+  "https://github.com/folke/persistence.nvim",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/ibhagwan/fzf-lua",
+  "https://github.com/j-hui/fidget.nvim",
+  "https://github.com/folke/lazydev.nvim",
+  "https://github.com/rafamadriz/friendly-snippets",
+  "https://github.com/saghen/blink.indent",
+  "https://github.com/saghen/blink.lib",
+  { src = "https://github.com/saghen/blink.pairs", version = "v0.6.0" },
+  { src = "https://github.com/saghen/blink.cmp",   version = "v1.10.2" },
 })
 
 require("vim._core.ui2").enable({})
 
-vim.cmd.colorscheme(vim.env.NVIM_COLORSCHEME or "catppuccin-latte")
-
-vim.lsp.enable("nixd")
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("rust_analyzer")
-vim.lsp.enable("zls")
-vim.lsp.enable("taplo")
-vim.lsp.enable("marksman")
-vim.lsp.enable("cfu")
-vim.lsp.enable("clangd")
-
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      diagnostics = { globals = { "vim" } },
-      workspace = { library = { vim.env.VIMRUNTIME } },
-    },
-  },
+require("nvim-treesitter").install({
+  "lua",
+  "rust",
+  "vim",
+  "vimdoc",
+  "markdown",
 })
 
-vim.lsp.config("cfu", {
-  cmd = { "cfu", "lsp", "--stdio" },
-  filetypes = { "json5" },
+require("blink.pairs").build()
+require("blink.cmp").setup({ keymap = { preset = "default" } })
+require("blink.pairs").setup({ highlights = { enabled = false } })
+require("blink.indent").setup({ scope = { char = "│" }, static = { char = "│" } })
+
+require("fidget").setup({})
+require("oil").setup({ default_file_explorer = true })
+require("lazydev").setup()
+require("fzf-lua").register_ui_select()
+require("live-rename").setup()
+require("persistence").setup()
+
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("lua_ls")
+
+vim.keymap.set("n", "<esc>", "<cmd>nohl<cr><esc>")
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("n", "<tab>", "<cmd>bn<cr>")
+vim.keymap.set("n", "<s-tab>", "<cmd>bp<cr>")
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>")
+vim.keymap.set({ "n", "i" }, "<C-k>", "<C-w>k")
+vim.keymap.set({ "n", "i" }, "<C-j>", "<C-w>j")
+vim.keymap.set({ "n", "i" }, "<C-h>", "<C-w>h")
+vim.keymap.set({ "n", "i" }, "<C-l>", "<C-w>l")
+vim.keymap.set({ "n" }, "s", function()
+  require("flash").jump()
+end)
+vim.keymap.set("n", "-", "<cmd>Oil<cr>")
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
+vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>")
+vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua live_grep<cr>")
+vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<cr>")
+vim.keymap.set("n", "grr", "<cmd>FzfLua lsp_references<cr>")
+vim.keymap.set("n", "gra", "<cmd>FzfLua lsp_code_actions<cr>")
+vim.keymap.set("n", "grn", function()
+  require("live-rename").rename()
+end)
+vim.keymap.set("n", "<leader>ql", function()
+  require("persistence").load({ last = true })
+end)
+
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -110,204 +123,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "*" },
-  callback = function(args)
-    if not pcall(vim.treesitter.start) then
-      return
-    end
-    local lang = vim.treesitter.language.get_lang(args.match)
-    if lang and vim.treesitter.query.get(lang, "indents") then
-      vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end
-  end,
-})
-
-require("treesitter-context").setup({
-  max_lines = 2,
-})
-
-require("nvim-treesitter-textobjects").setup({
-  select = { lookahead = true },
-  move = { set_jumps = true },
-})
-
--- Select
-local select = function(object)
-  return function()
-    require("nvim-treesitter-textobjects.select").select_textobject(object, "textobjects")
-  end
-end
-vim.keymap.set({ "x", "o" }, "af", select("@function.outer"))
-vim.keymap.set({ "x", "o" }, "if", select("@function.inner"))
-vim.keymap.set({ "x", "o" }, "ac", select("@class.outer"))
-vim.keymap.set({ "x", "o" }, "ic", select("@class.inner"))
-vim.keymap.set({ "x", "o" }, "aa", select("@parameter.outer"))
-vim.keymap.set({ "x", "o" }, "ia", select("@parameter.inner"))
-
--- Swap
-vim.keymap.set("n", "<leader>a", function()
-  require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
-end)
-
--- Move
-vim.keymap.set({ "n", "x", "o" }, "]f", function()
-  require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
-end)
-vim.keymap.set({ "n", "x", "o" }, "[f", function()
-  require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
-end)
-vim.keymap.set({ "n", "x", "o" }, "]a", function()
-  require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.outer", "textobjects")
-end)
-vim.keymap.set({ "n", "x", "o" }, "[a", function()
-  require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.outer", "textobjects")
-end)
-
-require("oil").setup({
-  default_file_explorer = true,
-  delete_to_trash = true,
-})
-
-require("flash").setup()
-vim.keymap.set({ "n", "x", "o" }, "s", function()
-  require("flash").jump()
-end)
--- vim.keymap.set({ "n", "x", "o" }, "r", function()
---   require("flash").remote()
--- end)
-
-require("sidekick").setup({})
-vim.keymap.set({ "n", "x" }, "<leader>aa", function()
-  require("sidekick.cli").toggle()
-end)
-vim.keymap.set({ "n", "x" }, "<leader>ad", function()
-  require("sidekick.cli").close()
-end)
-vim.keymap.set({ "n", "x" }, "<leader>at", function()
-  require("sidekick.cli").send({ msg = "{this}" })
-end)
-vim.keymap.set({ "n", "x" }, "<leader>af", function()
-  require("sidekick.cli").send({ msg = "{file}" })
-end)
-vim.keymap.set({ "n", "x" }, "<leader>av", function()
-  require("sidekick.cli").send({ msg = "{selection}" })
-end)
-vim.keymap.set({ "n", "x" }, "<leader>ap", function()
-  require("sidekick.cli").prompt()
-end)
-
-require("live-rename").setup()
-vim.keymap.set("n", "grn", function()
-  require("live-rename").rename()
-end)
-
-require("mini.extra").setup()
-require("mini.pick").setup({
-  window = {
-    config = function()
-      return { width = vim.o.columns }
-    end,
-  },
-  mappings = {
-    toggle_explorer = {
-      char = "<C-e>",
-      func = function()
-        local source = MiniPick.get_picker_opts().source
-        local cwd, was_explorer = source.cwd, source.name == "File explorer"
-        vim.schedule(function()
-          if was_explorer then
-            MiniPick.builtin.files(nil, { source = { cwd = cwd } })
-          else
-            MiniExtra.pickers.explorer({ cwd = cwd })
-          end
-        end)
-        return true
-      end,
-    },
-    wipeout = {
-      char = "<C-d>",
-      func = function()
-        local item = MiniPick.get_picker_matches().current
-        if type(item) == "table" and item.bufnr then
-          vim.api.nvim_buf_delete(item.bufnr, {})
-          MiniPick.set_picker_items(MiniPick.get_picker_items(), { do_match = true })
-        end
-      end,
-    },
-  },
-})
-vim.ui.select = MiniPick.ui_select
-
-local function workspace()
-  return vim.fs.root(0, ".git") or vim.fn.getcwd()
-end
-
--- Buffer name is empty when unnamed and a URI for oil:// buffers.
-local function buf_dir()
-  local name = vim.api.nvim_buf_get_name(0)
-  local dir = name ~= "" and vim.fs.dirname(name) or vim.fn.getcwd()
-  return vim.fn.isdirectory(dir) == 1 and dir or vim.fn.getcwd()
-end
-
-vim.keymap.set("n", "-", "<cmd>Oil<cr>")
-vim.keymap.set("n", "<leader>e", function()
-  MiniExtra.pickers.explorer({ cwd = buf_dir() })
-end)
-vim.keymap.set("n", "<leader>E", function()
-  MiniExtra.pickers.explorer({ cwd = workspace() })
-end)
-vim.keymap.set("n", "<leader>ff", function()
-  MiniPick.builtin.files(nil, { source = { cwd = workspace() } })
-end)
-vim.keymap.set("n", "<leader>fr", function()
-  MiniExtra.pickers.oldfiles()
-end)
-vim.keymap.set("n", "<leader>fb", function()
-  MiniPick.builtin.buffers()
-end)
-vim.keymap.set("n", "<leader>sg", function()
-  MiniPick.builtin.grep_live()
-end)
-vim.keymap.set("n", "<leader>ss", function()
-  MiniExtra.pickers.lsp({ scope = "document_symbol" })
-end)
-vim.keymap.set("n", "<leader>sS", function()
-  MiniExtra.pickers.lsp({ scope = "workspace_symbol" })
-end)
-vim.keymap.set("n", "<leader>sR", function()
-  MiniPick.builtin.resume()
-end)
-vim.keymap.set("n", "<leader>d", function()
-  MiniExtra.pickers.diagnostic({ scope = "all" })
-end)
-vim.keymap.set("n", "grr", function()
-  MiniExtra.pickers.lsp({ scope = "references" })
-end)
-vim.keymap.set("n", "gd", function()
-  MiniExtra.pickers.lsp({ scope = "definition" })
-end)
-
-require("blink.cmp").setup({})
-require("blink.pairs").setup({ highlights = { enabled = false } })
-require("blink.indent").setup({
-  scope = { char = "│" },
-  static = { char = "│" },
-})
-
-require("persistence").setup()
-vim.keymap.set("n", "<leader>ql", function()
-  require("persistence").load({ last = true })
-end)
-
-vim.keymap.set("t", "<C-k>", "<C-w>k")
-vim.keymap.set("t", "<C-j>", "<C-w>j")
-vim.keymap.set("t", "<C-h>", "<C-w>h")
-vim.keymap.set("t", "<C-l>", "<C-w>l")
-
-if vim.g.neovide then
-  vim.fn.chdir("~")
-  vim.g.neovide_cursor_animation_length = 0.05
-  vim.g.neovide_scroll_animation_length = 0.15
-end
